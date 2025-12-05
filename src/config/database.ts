85bbae28fb74f1006733879de08be8fdb54fd399
unknown <arifk62901@gmail.com>
2025-12-05 11:59:48 +0600

    users vehicles and bookings table added

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const pool = new Pool({
   connectionString: config.connection_uri as string,
 });
-// 3 table -> users, vehicles, bookings
+// 3 table -> [users], [vehicles], bookings
 export const initalizeDatabase = async () => {
   try {
     await pool.query(
@@ -18,6 +18,32 @@ export const initalizeDatabase = async () => {
           role VARCHAR(50) check(role IN('admin','customer'))
         )
       `
+    ),
+      await pool.query(
+        ` 
+        CREATE TABLE IF NOT EXISTS vehicles(
+            id SERIAL PRIMARY KEY,
+            vehicle_name VARCHAR(50) NOT NULL,
+            type VARCHAR(100) check(type IN('car','bike','van','SUV')),
+            registration_number VARCHAR(100) UNIQUE NOT NULL,
+            daily_rent_price INT NOT NULL check(daily_rent_price > 0),
+            availability_status VARCHAR(50) check(availability_status IN('available','booked'))
+        )
+      `
+      );
+    await pool.query(
+      `
+        CREATE TABLE IF NOT EXISTS bookings (
+        id SERIAL PRIMARY KEY,
+        customer_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+        vehicle_id INT NOT NULL REFERENCES vehicles(id) ON DELETE CASCADE, 
+        rent_start_date DATE NOT NULL,
+        rent_end_date DATE NOT NULL,
+        total_price INT NOT NULL check(total_price > 0),
+        status VARCHAR(50) check(status IN('active','cancelled','returned')),
+        check(rent_start_date < rent_end_date)
+        )
+        `
     );
     console.log("database table added");
   } catch (error: any) {
